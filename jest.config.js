68af5c6ae003ada2150d170116e0b3d28e3642dd
Julian Waller <git@julusian.co.uk>
2022-10-09 16:03:22 +0100

    chore: Initial commit

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+	moduleFileExtensions: ['ts', 'js', 'json'],
+	transform: {
+		'^.+\\.(ts|tsx)$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.json',
+				diagnostics: false,
+			},
+		],
+	},
+	testMatch: ['**/__tests__/**/*.spec.(ts|js)'],
+	testPathIgnorePatterns: ['integrationTests'],
+	testEnvironment: 'node',
+	coverageThreshold: {
+		global: {
+			branches: 0,
+			functions: 0,
+			lines: 0,
+			statements: 0,
+		},
+	},
+	collectCoverageFrom: [
+		'**/src/**/*.{ts,js}',
+		'!**/src/__tests__/**',
+		'!**/node_modules/**',
+		'!**/dist/**',
+		'!packages/webhid-demo/**',
+	],
+	collectCoverage: true,
+	projects: ['<rootDir>'],
+	coverageDirectory: '<rootDir>/coverage/',
+
+	preset: 'ts-jest',
+
+	moduleNameMapper: {
+		'@elgato-stream-deck/(.+)': '<rootDir>/packages/$1/src',
+	},
+}
---
+++ packages/node/README.md
@@ -0,0 +1,1 @@
+# @loupedeck/node
